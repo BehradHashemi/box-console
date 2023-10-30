9dc6bc07dfd703459b7083e5085c1511aca87034
BehradHashemi <behradweb07@gmail.com>
2023-10-30 16:48:52 +0330

    org source

---
+++ app.js
@@ -0,0 +1,22 @@
+// فرمت
+// %s
+// %i, %d
+// % f
+// %o
+// %O
+// %c	   
+
+// سینتکس
+console.log("%c FronckTeam !", "color:red");
+
+// فونت
+console.log("%c FronckTeam !", "color: aqua; font-size: 20px");
+
+console.log("%c FronckTeam !", "color: aqua; font-family:serif; font-size: 20px");
+
+console.log("%c FronckTeam !", "color: aqua; font-family:monospace; font-size: 20px");
+
+console.log("%c FronckTeam !", "color: aqua; font-family:sans-serif; font-size: 20px");
+
+// ترکیب و ادغام همه استایل
+console.log('%c FronckTeam !', 'font-family:monospace; font-weight: bold; font-size: 50px;color: white; text-shadow: 3px 3px 0 #000 , 6px 6px 0 #fff , 9px 9px 0 #000 , 12px 12px 0 #fff , 15px 15px 0 #000 , 18px 18px 0 #fff , 21px 21px 0 #000');
